1236b1d15d05b84bfab79d5e2ff7e201c8911fa1
Dmytro Ushchenko <dmytro.ushchenko@vaimo.com>
2025-09-20 21:56:12 +0300

    add api.js

---
+++ fe_part/src/config/api.js
@@ -0,0 +1,12 @@
+const API_BASE_URL = 'http://localhost:3010';
+
+// Debug logging
+console.log('Environment VUE_APP_API_URL:', process.env.VUE_APP_API_URL);
+console.log('Final API_BASE_URL:', API_BASE_URL);
+
+export default {
+	API_BASE_URL
+};
+
+
+
